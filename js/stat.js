4768cf473b9af1019b4cf905e35a468dd7054e6b
aobrazovskaya <anna.obrazovskaya@gmail.com>
2019-09-17 22:34:27 +0300

    Добавлен заголовок гистограммы

--- js/stat.js
+++ js/stat.js
@@ -5,10 +5,11 @@ var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var GAP = 10;
+var TITLE_GAP = 35;
 var COLUMN_GAP = 50;
-var TEXT_HEIGHT = 25;
-var BAR_WIDTH = 40;
-var BAR_HEIGHT = 150;
+var TEXT_HEIGHT = 20;
+var COLUMN_WIDTH = 40;
+var COLUMN_HEIGHT = 130;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -31,8 +32,12 @@ window.renderStatistics = function (ctx, players, times) {
   ctx.fillStyle = '#000';
   var maxTime = getMaxElement(times);
   for (var i = 0; i < players.length; i++) {
-    var barHeight = -BAR_HEIGHT * times[i] / maxTime;
-    ctx.fillText(players[i], CLOUD_X + COLUMN_GAP + (COLUMN_GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - GAP);
-    ctx.fillRect(CLOUD_X + COLUMN_GAP + (COLUMN_GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - GAP - TEXT_HEIGHT, BAR_WIDTH, barHeight);
+    var barHeight = -COLUMN_HEIGHT * times[i] / maxTime;
+    ctx.fillText(players[i], CLOUD_X + COLUMN_GAP + (COLUMN_GAP + COLUMN_WIDTH) * i, CLOUD_HEIGHT - GAP);
+    ctx.fillRect(CLOUD_X + COLUMN_GAP + (COLUMN_GAP + COLUMN_WIDTH) * i, CLOUD_HEIGHT - GAP - TEXT_HEIGHT, COLUMN_WIDTH, barHeight);
+    ctx.fillText(Math.round(times[i]), CLOUD_X + COLUMN_GAP + (COLUMN_GAP + COLUMN_WIDTH) * i, CLOUD_HEIGHT - GAP - TEXT_HEIGHT + barHeight - GAP, COLUMN_WIDTH);
+    ctx.font = '16 px, PT Mono';
+    ctx.fillText('Ура вы победили!', CLOUD_X + TITLE_GAP, CLOUD_Y + TITLE_GAP);
+    ctx.fillText('Список результатов:', CLOUD_X + TITLE_GAP, CLOUD_Y + TITLE_GAP + TEXT_HEIGHT);
   }
 };
