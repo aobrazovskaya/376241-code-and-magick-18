b78f22fceb8b44e4f1b83b1d954b181e2a5da356
aobrazovskaya <anna.obrazovskaya@gmail.com>
2019-09-23 21:54:25 +0300

    Создана функция получения рандомного числа и массив объектов

--- js/setup.js
+++ js/setup.js
@@ -5,18 +5,28 @@ setup.classList.remove('hidden');
 
 var charactersFirstNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var charactersLastNames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var coarColors = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
+var coatColors = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
 var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
 
-var characters = [];
-var getRandomNumberFromArray = function (arr) {
-  Math.floor(Math.random * arr.length);
+/**
+ * Get random number with min value and max value.
+ * @param {number} min min value
+ * @param {number} max max value
+ * @return {number} random number in the range
+ */
+var getRandomNumberInTheRange = function (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
 };
 
+var characters = [];
 for (var i = 0; i < 4; i++) {
+  var randomNumber = getRandomNumberInTheRange(0, charactersFirstNames.length);
   var character = {
-    name: charactersFirstNames[Math.floor(Math.random * charactersFirstNames.length)] + charactersLastNames[Math.floor(Math.random * charactersLastNames.length)];
-  }
-  characters[name] = 
-  
-};
+    name: charactersFirstNames[randomNumber] + ' ' + charactersLastNames[randomNumber],
+    coatColor: coatColors[getRandomNumberInTheRange(0, coatColors.length)],
+    eyesColor: eyesColors[getRandomNumberInTheRange(0, eyesColors.length)],
+  };
+  characters.push(character);
+}
