ff81dcd7eab5863ae4e42cf5aee43d85423b4e64
aobrazovskaya <anna.obrazovskaya@gmail.com>
2019-09-25 21:51:24 +0300

    Испралены замечания и добавлены обертки кода

--- js/setup.js
+++ js/setup.js
@@ -1,39 +1,33 @@
 'use strict';
 
-var setup = document.querySelector('.setup');
-setup.classList.remove('hidden');
-document.querySelector('.setup-similar').classList.remove('hidden');
-
+var wizardsNumber = 4;
 var wizardsFirstNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var wizardsLastNames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
 
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
 /**
- * Get random number with min value and max value.
+ * Setup the initial state of the page with wizards.
+ */
+var setup = function () {
+  setup = document.querySelector('.setup');
+  setup.querySelector('.setup-similar').classList.remove('hidden');
+  setup.classList.remove('hidden');
+};
+
+/**
  * @param {number} min min value
  * @param {number} max max value
- * @return {number} random number in the range
+ * @return {number}
  */
 var getRandomNumberInTheRange = function (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
 };
-
-var wizards = [];
-for (var i = 0; i < 4; i++) {
-  var randomNumber = getRandomNumberInTheRange(0, wizardsFirstNames.length);
-  var character = {
-    name: wizardsFirstNames[randomNumber] + ' ' + wizardsLastNames[randomNumber],
-    coatColor: coatColors[getRandomNumberInTheRange(0, coatColors.length)],
-    eyesColor: eyesColors[getRandomNumberInTheRange(0, eyesColors.length)],
-  };
-  wizards.push(character);
-}
-
-var similarListElement = document.querySelector('.setup-similar-list');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
 /**
  * Drawing a wizard with changing some parameters.
@@ -49,10 +43,28 @@ var renderWizard = function (wizard) {
   return wizardElement;
 };
 
-var fragment = document.createDocumentFragment();
-for (i = 0; i < 4; i++) {
-  fragment.appendChild(renderWizard(wizards[i]));
-}
-similarListElement.appendChild(fragment);
+var createWizardsWithAttributes = function () {
+  var wizards = [];
+  for (var i = 0; i < wizardsNumber; i++) {
+    var randomNumber = getRandomNumberInTheRange(0, wizardsFirstNames.length);
+    var character = {
+      name: wizardsFirstNames[randomNumber] + ' ' + wizardsLastNames[randomNumber],
+      coatColor: coatColors[getRandomNumberInTheRange(0, coatColors.length)],
+      eyesColor: eyesColors[getRandomNumberInTheRange(0, eyesColors.length)],
+    };
+    wizards.push(character);
+  }
+  return wizards;
+};
 
-setup.querySelector('.setup-similar').classList.remove('hidden');
+var createFragment = function () {
+  var wizards = createWizardsWithAttributes();
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < wizardsNumber; i++) {
+    fragment.appendChild(renderWizard(wizards[i]));
+  }
+  similarListElement.appendChild(fragment);
+};
+
+createFragment();
+setup();
